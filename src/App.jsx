9d5980550098627d2d7f86ff76f73198580a77cc
Carlos Padilla <carlosfpm-@hotmail.com>
2025-04-09 20:48:44 -0500

    Add route for page animals for adoption

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import SignUp from './pages/SignUp.jsx'
+import AnimalesAdopcion from './pages/AnimalesAdopcion.jsx'
 import './App.css'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<Home />} />
+        <Route path="/animales-adopcion" element={<AnimalesAdopcion />} />
       </Routes>
     </Router>
   )
