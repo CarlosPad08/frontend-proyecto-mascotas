c207b655c6ac5ed0ccb241a179e991257d57c1e5
JuanCamiloGutiV <juan.gutierrez.viveroscorreounivalle.edu.co>
2025-05-17 00:15:44 -0500

    SCRUM 90 - página 404 finalizada e implementada

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import '../styles/notfound.css';
+import { useNavigate } from 'react-router-dom';
 
 const NotFound = () => {
+  const navigate = useNavigate();
+  const handleBack = () => {
+    navigate (-1);
+  }
   return (
     <div className="notfound-container">
+      <button className="back-button" onClick={handleBack}>Regresar</button>
       <h1 className="notfound-title">Error 404</h1>
       <p className="notfound-message">Página no encontrada :/</p>
       <p className="notfound-message">¡No pierdas las huellas de tu próxima mascota!</p>
