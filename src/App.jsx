57ed5242a902e63791e4cdb84c0261e2480524a2
Carlos Padilla <carlosfpm-@hotmail.com>
2025-05-27 17:03:56 -0500

    add(guarderias): modulo de guarderias

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import SignUpRefugio from './pages/SignUpRefugio.jsx';
 import DashboardRefugio from './pages/DashboardRefugio.jsx';
 import NotFound from './pages/NotFound.jsx';
 import Veterinarias from './pages/Veterinarias.jsx';
+import Guarderias from './pages/Guarderias.jsx';
 import './App.css';
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
         <Route path="/signup-refugio" element={<SignUpRefugio />} />
         <Route path="/dashboard-refugio" element={<DashboardRefugio />} />
         <Route path="/veterinarias" element={<Veterinarias />} />
+        <Route path="/guarderias" element={<Guarderias />} />
         <Route path="/error-404" element={<NotFound/>} />
         <Route path="*" element={<NotFound/>} />
       </Routes>
